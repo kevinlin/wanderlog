b515a69e657305238df8b0a992f4be34ff26e6c7
Kevin Lin <kevin.lin@zuhlke.com>
2025-08-25 23:07:51 +0800

    Replaced activity flag icon with the MapPinIcon from Heroicons

--- src/components/Map/MapContainer.tsx
+++ src/components/Map/MapContainer.tsx
@@ -226,21 +226,21 @@ export const MapContainer: React.FC<MapContainerProps> = ({
     };
   };
 
-  // Activity pin (type-specific icons)
+  // Activity pin (MapPin icon from Heroicons)
   const getActivityPinIcon = (activityType: ActivityType, isSelected: boolean) => {
     const color = getActivityTypeColor(activityType);
     
-    // Default green flag for "Want to go" style (OTHER type)
+    // MapPin icon SVG path from Heroicons
     const iconUrl = `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(`
       <svg width="24" height="24" viewBox="0 0 24 24" fill="${color}" xmlns="http://www.w3.org/2000/svg">
-        <path d="M14.4 6L14 4H5v17h2v-7h5.6l.4 2h7V6z"/>
+        <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
       </svg>
     `)}`;
 
     return {
       url: iconUrl,
-      scaledSize: new window.google.maps.Size(isSelected ? 24 : 18, isSelected ? 24 : 18),
-      anchor: new window.google.maps.Point(isSelected ? 12 : 9, isSelected ? 24 : 18),
+      scaledSize: new window.google.maps.Size(isSelected ? 28 : 20, isSelected ? 28 : 20),
+      anchor: new window.google.maps.Point(isSelected ? 14 : 10, isSelected ? 28 : 20),
     };
   };
 
